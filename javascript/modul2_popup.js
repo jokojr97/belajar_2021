3bfd1737c08306b9e90b0c350e5eaa12e0d45713
jokojr97 <joko.riyadi97@gmail.com>
2021-01-12 18:13:20 +0700

    penguatan Javascript

---
+++ javascript/modul2_popup.js
@@ -0,0 +1,14 @@
+// alert
+// alert('Hello World');
+
+// prompt
+// var nama = prompt('Masukkan Nama: ');
+// alert(nama);
+
+// confirm
+var konfirmasi = confirm('Apakah Anda Yakin?');
+if (konfirmasi == true){
+    alert('Ya, saya yakin!');
+}else {
+    alert('tidak, saya tidak yakin!');
+}
